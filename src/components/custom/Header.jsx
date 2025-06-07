ec5b35d1027675c53e385ff3a1da7c42b12f3ef4
Aayushsinghal294 <23bcs003@iiitdmj.ac.in>
2025-06-07 09:35:25 +0530

    Dashboard setup done

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
+import { UserButton, useUser } from '@clerk/clerk-react';
 
 const Header = () => {
+  const {user,isSignedIn} = useUser();
   return (
-    <div>
-      <img src="/logo.svg" width={100} height={100} />
+    <div className=' px-7 flex justify-between shadow-md bg-black'>
+      <img src="/logo.svg" width={150} />
+
+{
+  isSignedIn?
+  <div className='flex gap-2 items-center'>
+    <Link to={'/dashboard'}>
+    <Button variant="outline">Dashboard</Button>
+    </Link>
+    <UserButton/></div>:
+    
+      <Link to={'/auth/sign-in'}>
+      <Button className='mt-7 mr-5'>Get Started</Button>
+      </Link>
+}
+
+
     </div>
   )
 }
